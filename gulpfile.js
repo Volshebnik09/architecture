6f672904cc661afb59f0478cbfe52e2a5b9a48c0
Komnatniy_Koldun <82974466+Volshebnik09@users.noreply.github.com>
2022-05-13 13:25:17 +0300

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { src, dest, task, watch } = require('gulp');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 updatePug = () => {
   return src('./src/pages/*.pug')
@@ -13,10 +14,18 @@ updatePug = () => {
 };
 
 function buildStyles() {
-  return src('./src/styles/*.scss')
+  return src('./src/styles/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('./dist/styles'));
 };
+exports.imageMinify = ()=>{
+  return src('src/images/*')
+      .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.mozjpeg({quality: 75, progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+      ])).pipe(dest('dist/images'))
+}
 
 task('default', function () {
     watch('./src/pages/**/*.pug', updatePug);
